71bd44ce201a2007ac1e7dc4d2bdf37731abd0dd
Zhong Dai <zhong.dai@rokt.com>
2026-03-01 19:25:52 -0500

    chore: scaffold project with TS, Webpack, Jest, ESLint

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    popup: './src/popup.ts',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: 'src/manifest.json', to: 'manifest.json' },
+        { from: 'src/popup.html', to: 'popup.html' },
+        { from: 'src/popup.css', to: 'popup.css' },
+      ],
+    }),
+  ],
+};
